284b0b29d236872a629d7a1446c31b746dfe222e
AlinaSachuk <alinko-91@yandex.ru>
2023-03-27 10:34:54 +0300

    Hibernate: added to Employee class.

--- src/main/java/com/beauty_project/domain/Employee.java
+++ src/main/java/com/beauty_project/domain/Employee.java
@@ -3,11 +3,27 @@ package com.beauty_project.domain;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.*;
+
 @Data
 @Component
+@Entity
+@Table(name = "employee_table")
 public class Employee {
-    private String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "employee_id_seq_gen")
+    @SequenceGenerator(name = "employee_id_seq_gen", sequenceName = "employee_table_id_seq", allocationSize = 1)
+    private int id;
+
+    @Column(name = "name")
+    private String employeeName;
+
+    @Column(name = "position")
     private String position;
+
+    @Column(name = "education")
     private String education;
+
+    @Column(name = "work_experience")
     private String workExperience;
 }
