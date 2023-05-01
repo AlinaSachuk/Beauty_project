0f12c1a06d9735b154b609966e5e265151f1467a
AlinaSachuk <alinko-91@yandex.ru>
2023-05-01 22:31:55 +0300

    notFound exception added to customer service in case of unknown customer.

--- src/main/java/com/beauty_project/service/CustomerService.java
+++ src/main/java/com/beauty_project/service/CustomerService.java
@@ -4,6 +4,7 @@ import com.beauty_project.domain.Customer;
 import com.beauty_project.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.webjars.NotFoundException;
 
 import java.util.ArrayList;
 import java.util.Optional;
@@ -30,10 +31,12 @@ public class CustomerService {
     }
 
     public Customer updateCustomer(Customer customer) {
+        customerRepository.findById(customer.getId()).orElseThrow(() -> new NotFoundException("Customer with id=" + customer.getId() + " not found"));
         return customerRepository.save(customer);
     }
 
     public void deleteCustomer(int id) {
+        customerRepository.findById(id).orElseThrow(() -> new NotFoundException("Customer with id=" + id + " not found"));
         customerRepository.deleteById(id);
     }
 }
