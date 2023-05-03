301cac9da4a40d4c843d3bc834feeaf3bc151ad3
AlinaSachuk <alinko-91@yandex.ru>
2023-05-03 10:22:33 +0300

    warning logs added to procedure controller (get and post methods).

--- src/main/java/com/beauty_project/controller/ProcedureController.java
+++ src/main/java/com/beauty_project/controller/ProcedureController.java
@@ -2,6 +2,8 @@ package com.beauty_project.controller;
 
 import com.beauty_project.domain.Procedure;
 import com.beauty_project.service.ProcedureService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +23,7 @@ import java.util.Optional;
 @RequestMapping("/procedure")
 public class ProcedureController {
     ProcedureService procedureService;
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     public ProcedureController(ProcedureService procedureService) {
@@ -31,6 +34,7 @@ public class ProcedureController {
     public ResponseEntity<Optional<Procedure>> getProcedureById(@PathVariable int id) {
         Optional<Procedure> procedure = procedureService.getProcedureById(id);
         if (procedure.isEmpty()) {
+            log.warn("Procedure with id=" + id + " not found: getProcedureById method.");
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(procedure, HttpStatus.OK);
@@ -39,12 +43,19 @@ public class ProcedureController {
     @GetMapping
     public ResponseEntity<ArrayList<Procedure>> getAllProcedures() {
         ArrayList<Procedure> procedureArrayList = procedureService.getAllProcedures();
+        if (procedureArrayList.isEmpty()) {
+            log.warn("Something went wrong in getAllProcedures method.");
+        }
         return new ResponseEntity<>(procedureArrayList, (!procedureArrayList.isEmpty()) ? HttpStatus.OK : HttpStatus.NOT_FOUND);
     }
 
     @PostMapping
     public ResponseEntity<HttpStatus> createProcedure(@RequestBody Procedure procedure) {
         procedureService.createProcedure(procedure);
+        if (procedure == null) {
+            log.warn("Something went wrong: procedure not created");
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
