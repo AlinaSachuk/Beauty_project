78e0e4884f5567bffe5c1d0efa9b4d2a9d1bf9ff
AlinaSachuk <alinko-91@yandex.ru>
2023-05-05 00:18:28 +0300

    Tests: created tests for Employee.

---
+++ src/test/java/com/beauty_project/service/EmployeeServiceImplTest.java
@@ -0,0 +1,50 @@
+package com.beauty_project.service;
+
+import com.beauty_project.domain.Employee;
+import com.beauty_project.domain.Status;
+import com.beauty_project.repository.EmployeeRepository;
+import com.beauty_project.service.impl.EmployeeServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.webjars.NotFoundException;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doThrow;
+
+@ExtendWith(MockitoExtension.class)
+public class EmployeeServiceImplTest {
+    @Mock
+    private EmployeeRepository employeeRepository;
+    @InjectMocks
+    private EmployeeServiceImpl employeeService;
+    private int id;
+    private Employee employee;
+    private ArrayList<Employee> allEmployees;
+
+    @Test
+    public void testGetByIdWithNotFoundException() {
+        doThrow(NotFoundException.class).when(employeeRepository).findById(id);
+        assertThrows(NotFoundException.class, () -> employeeService.getEmployeeById(id));
+    }
+
+    @Test
+    public void testGetAllEmployees() {
+        when(employeeRepository.findAll()).thenReturn(allEmployees);
+        ArrayList<Employee> returnedEmployees = employeeService.getAllEmployees();
+        verify(employeeRepository).findAll();
+        assertEquals(allEmployees, returnedEmployees);
+    }
+    @Test
+    public void testDeleteEmployeeByIdWithEmptyResultDataAccessException() {
+        doThrow(EmptyResultDataAccessException.class).when(employeeRepository).deleteById(id);
+        assertThrows(EmptyResultDataAccessException.class, () -> employeeService.deleteEmployee(id));
+    }
+}
