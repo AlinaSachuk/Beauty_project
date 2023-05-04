1057350e1a84722534f94813a0b0a191df4df241
AlinaSachuk <alinko-91@yandex.ru>
2023-05-05 00:38:36 +0300

    Tests: created tests for Procedure.

---
+++ src/test/java/com/beauty_project/service/ProcedureServiceImplTest.java
@@ -0,0 +1,50 @@
+package com.beauty_project.service;
+
+import com.beauty_project.domain.Procedure;
+import com.beauty_project.repository.ProcedureRepository;
+import com.beauty_project.service.impl.ProcedureServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.webjars.NotFoundException;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ProcedureServiceImplTest {
+    @Mock
+    private ProcedureRepository procedureRepository;
+    @InjectMocks
+    private ProcedureServiceImpl procedureService;
+    private int id;
+    private Procedure procedure;
+    private ArrayList<Procedure> allProcedures;
+
+    @Test
+    public void testGetByIdWithNotFoundException() {
+        doThrow(NotFoundException.class).when(procedureRepository).findById(id);
+        assertThrows(NotFoundException.class, () -> procedureService.getProcedureById(id));
+    }
+
+    @Test
+    public void testGetAllProcedures() {
+        when(procedureRepository.findAll()).thenReturn(allProcedures);
+        ArrayList<Procedure> returnedProcedures = procedureService.getAllProcedures();
+        verify(procedureRepository).findAll();
+        assertEquals(allProcedures, returnedProcedures);
+    }
+    @Test
+    public void testDeleteProcedureByIdWithEmptyResultDataAccessException() {
+        doThrow(EmptyResultDataAccessException.class).when(procedureRepository).deleteById(id);
+        assertThrows(EmptyResultDataAccessException.class, () -> procedureService.deleteProcedure(id));
+    }
+}
