672360166d347425e5d23fe29f1bc0ca02cc700a
AlinaSachuk <alinko-91@yandex.ru>
2023-05-05 00:10:32 +0300

    Tests: created Util class; created tests for Status.

---
+++ src/test/java/com/beauty_project/Utils.java
@@ -0,0 +1,77 @@
+package com.beauty_project;
+
+import com.beauty_project.domain.CosmeticProduct;
+import com.beauty_project.domain.Customer;
+import com.beauty_project.domain.Employee;
+import com.beauty_project.domain.Procedure;
+import com.beauty_project.domain.Status;
+import com.beauty_project.domain.Visit;
+
+import java.sql.Date;
+
+public final class Utils {
+    private Utils() {
+    }
+
+    public static Status createStatus(String status, int percent, int id) {
+        Status testStatus = new Status();
+        testStatus.setStatus(status);
+        testStatus.setPercent(percent);
+        testStatus.setId(id);
+        return testStatus;
+    }
+
+    public static Procedure createProcedure(Integer id, String serviceName, int duration, int price, String description) {
+        Procedure procedure = new Procedure();
+        procedure.setId(id);
+        procedure.setServiceName(serviceName);
+        procedure.setDuration(duration);
+        procedure.setPrice(price);
+        procedure.setDescription(description);
+        return procedure;
+    }
+
+    public static Employee createEmployee(Integer id, String employeeName, String position
+            , String education, String workExperience) {
+        Employee employee = new Employee();
+        employee.setId(id);
+        employee.setEmployeeName(employeeName);
+        employee.setPosition(position);
+        employee.setEducation(education);
+        employee.setWorkExperience(workExperience);
+        return employee;
+    }
+
+    public static CosmeticProduct createCosmeticProduct(Integer id, String productName, String manufacture
+            , String countryOfOrigin) {
+        CosmeticProduct product = new CosmeticProduct();
+        product.setId(id);
+        product.setProductName(productName);
+        product.setManufacture(manufacture);
+        product.setCountryOfOrigin(countryOfOrigin);
+        return product;
+    }
+
+    public static Customer createCustomer(Integer id, String customerName, Date birthDate, String telephoneNumber
+            , String email, String instagramAccount, String status, String password) {
+        Customer customer = new Customer();
+        customer.setId(id);
+        customer.setCustomerName(customerName);
+        customer.setBirthDate(birthDate);
+        customer.setTelephoneNumber(telephoneNumber);
+        customer.setEmail(email);
+        customer.setInstagramAccount(instagramAccount);
+        customer.setStatus(status);
+        customer.setPassword(password);
+        return customer;
+    }
+
+    public static Visit createVisit(Integer id, Date dateOfVisit, double finalPrice, int customerId) {
+        Visit visit = new Visit();
+        visit.setId(id);
+        visit.setDateOfVisit(dateOfVisit);
+        visit.setFinalPrice(finalPrice);
+        visit.setCustomerId(customerId);
+        return visit;
+    }
+}
---
+++ src/test/java/com/beauty_project/service/StatusServiceImplTest.java
@@ -0,0 +1,53 @@
+package com.beauty_project.service;
+
+import com.beauty_project.domain.Status;
+import com.beauty_project.domain.dto.CreateUpdateStatusDto;
+import com.beauty_project.mappers.StatusMapper;
+import com.beauty_project.repository.StatusRepository;
+import com.beauty_project.service.impl.StatusServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.webjars.NotFoundException;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class StatusServiceImplTest {
+    @Mock
+    private StatusRepository statusRepository;
+    @InjectMocks
+    private StatusServiceImpl statusService;
+    private int id;
+    private Status status;
+    private CreateUpdateStatusDto statusDto;
+    private ArrayList<Status> allStatus;
+
+    @Test
+    public void testGetByIdWithNotFoundException() {
+        doThrow(NotFoundException.class).when(statusRepository).findById(id);
+        assertThrows(NotFoundException.class, () -> statusService.getStatusById(id));
+    }
+
+    @Test
+    public void testGetAllStatus() {
+        when(statusRepository.findAll()).thenReturn(allStatus);
+        ArrayList<Status> returnedStatus = statusService.getAllStatus();
+        verify(statusRepository).findAll();
+        assertEquals(allStatus, returnedStatus);
+    }
+    @Test
+    public void testDeleteStatusByIdWithEmptyResultDataAccessException() {
+        doThrow(EmptyResultDataAccessException.class).when(statusRepository).deleteById(id);
+        assertThrows(EmptyResultDataAccessException.class, () -> statusService.deleteStatus(id));
+    }
+}
