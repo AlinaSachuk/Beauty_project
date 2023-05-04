6d3db48bf1eff4bc79a4a3d244f135d402267e9a
AlinaSachuk <alinko-91@yandex.ru>
2023-05-05 00:32:48 +0300

    Tests: created tests for Customer.

---
+++ src/test/java/com/beauty_project/service/CustomerServiceImplTest.java
@@ -0,0 +1,50 @@
+package com.beauty_project.service;
+
+import com.beauty_project.domain.Customer;
+import com.beauty_project.repository.CustomerRepository;
+import com.beauty_project.service.impl.CustomerServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.webjars.NotFoundException;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class CustomerServiceImplTest {
+    @Mock
+    private CustomerRepository customerRepository;
+    @InjectMocks
+    private CustomerServiceImpl customerService;
+    private int id;
+    private Customer customer;
+    private ArrayList<Customer> allCustomers;
+
+    @Test
+    public void testGetByIdWithNotFoundException() {
+        doThrow(NotFoundException.class).when(customerRepository).findById(id);
+        assertThrows(NotFoundException.class, () -> customerService.getCustomerById(id));
+    }
+
+    @Test
+    public void testGetAllCustomers() {
+        when(customerRepository.findAll()).thenReturn(allCustomers);
+        ArrayList<Customer> returnedCustomers = customerService.getAllCustomers();
+        verify(customerRepository).findAll();
+        assertEquals(allCustomers, returnedCustomers);
+    }
+    @Test
+    public void testDeleteCustomerByIdWithEmptyResultDataAccessException() {
+        doThrow(EmptyResultDataAccessException.class).when(customerRepository).deleteById(id);
+        assertThrows(EmptyResultDataAccessException.class, () -> customerService.deleteCustomer(id));
+    }
+}
