73e7d503071b10876f04f330771fe0d4efbb496c
AlinaSachuk <alinko-91@yandex.ru>
2023-05-07 13:40:55 +0300

    Tests: get, getAll, delete for Procedure created.

--- src/test/java/com/beauty_project/service/ProcedureServiceImplTest.java
+++ src/test/java/com/beauty_project/service/ProcedureServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.beauty_project.service;
 
+import com.beauty_project.Utils;
 import com.beauty_project.domain.Procedure;
 import com.beauty_project.repository.ProcedureRepository;
 import com.beauty_project.service.impl.ProcedureServiceImpl;
@@ -11,11 +12,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.webjars.NotFoundException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -25,26 +28,35 @@ public class ProcedureServiceImplTest {
     private ProcedureRepository procedureRepository;
     @InjectMocks
     private ProcedureServiceImpl procedureService;
-    private int id;
-    private Procedure procedure;
-    private List<Procedure> allProcedures;
 
     @Test
     public void testGetByIdWithNotFoundException() {
-        doThrow(NotFoundException.class).when(procedureRepository).findById(id);
-        assertThrows(NotFoundException.class, () -> procedureService.getProcedureById(id));
+        doThrow(NotFoundException.class).when(procedureRepository).findById(1);
+        assertThrows(NotFoundException.class, () -> procedureService.getProcedureById(1));
     }
 
     @Test
     public void testGetAllProcedures() {
+        Procedure procedure1 = Utils.createProcedure(1, "Massage", 60, 25, "relax facial massage");
+        Procedure procedure2 = Utils.createProcedure(2, "Massage", 60, 25, "relax facial massage");
+        List<Procedure> allProcedures = new ArrayList<>();
+        allProcedures.add(procedure1);
+        allProcedures.add(procedure2);
         when(procedureRepository.findAll()).thenReturn(allProcedures);
-        List<Procedure> returnedProcedures = procedureService.getAllProcedures();
-        verify(procedureRepository).findAll();
-        assertEquals(allProcedures, returnedProcedures);
+        List<Procedure> actual = procedureRepository.findAll();
+        verify(procedureRepository, times(1)).findAll();
+        assertEquals(2, actual.size());
     }
+
+    @Test
+    public void testDeleteProcedureById() {
+        procedureService.deleteProcedure(1);
+        verify(procedureRepository, times(1)).deleteById(1);
+    }
+
     @Test
     public void testDeleteProcedureByIdWithEmptyResultDataAccessException() {
-        doThrow(EmptyResultDataAccessException.class).when(procedureRepository).deleteById(id);
-        assertThrows(EmptyResultDataAccessException.class, () -> procedureService.deleteProcedure(id));
+        doThrow(EmptyResultDataAccessException.class).when(procedureRepository).deleteById(1);
+        assertThrows(EmptyResultDataAccessException.class, () -> procedureService.deleteProcedure(1));
     }
 }
