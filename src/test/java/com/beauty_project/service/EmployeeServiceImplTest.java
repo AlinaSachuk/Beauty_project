136ea33871125ba730ae6123ceba14aab5dd3351
AlinaSachuk <alinko-91@yandex.ru>
2023-05-07 13:28:15 +0300

    Tests: get, getAll, delete for Employee created.

--- src/test/java/com/beauty_project/service/EmployeeServiceImplTest.java
+++ src/test/java/com/beauty_project/service/EmployeeServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.beauty_project.service;
 
+import com.beauty_project.Utils;
 import com.beauty_project.domain.Employee;
 import com.beauty_project.repository.EmployeeRepository;
 import com.beauty_project.service.impl.EmployeeServiceImpl;
@@ -11,13 +12,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.webjars.NotFoundException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class EmployeeServiceImplTest {
@@ -25,27 +25,35 @@ public class EmployeeServiceImplTest {
     private EmployeeRepository employeeRepository;
     @InjectMocks
     private EmployeeServiceImpl employeeService;
-    private int id;
-    private Employee employee;
-    private List<Employee> allEmployees;
 
     @Test
     public void testGetByIdWithNotFoundException() {
-        doThrow(NotFoundException.class).when(employeeRepository).findById(id);
-        assertThrows(NotFoundException.class, () -> employeeService.getEmployeeById(id));
+        doThrow(NotFoundException.class).when(employeeRepository).findById(1);
+        assertThrows(NotFoundException.class, () -> employeeService.getEmployeeById(1));
     }
 
     @Test
     public void testGetAllEmployees() {
-        when(employeeRepository.findAll()).thenReturn(allEmployees);
-        List<Employee> returnedEmployees = employeeService.getAllEmployees();
-        verify(employeeRepository).findAll();
-        assertEquals(allEmployees, returnedEmployees);
+        Employee employee1 = Utils.createEmployee(1, "Anna", "doctor", "BSMU", "since 2015");
+        Employee employee2 = Utils.createEmployee(2, "Anna", "doctor", "BSMU", "since 2015");
+        List<Employee> returnedEmployees = new ArrayList<>();
+        returnedEmployees.add(employee1);
+        returnedEmployees.add(employee2);
+        when(employeeRepository.findAll()).thenReturn(returnedEmployees);
+        List<Employee> actual = employeeRepository.findAll();
+        verify(employeeRepository, times(1)).findAll();
+        assertEquals(2, actual.size());
+    }
+
+    @Test
+    public void testDeleteEmployeeById() {
+        employeeService.deleteEmployee(1);
+        verify(employeeRepository, times(1)).deleteById(1);
     }
 
     @Test
     public void testDeleteEmployeeByIdWithEmptyResultDataAccessException() {
-        doThrow(EmptyResultDataAccessException.class).when(employeeRepository).deleteById(id);
-        assertThrows(EmptyResultDataAccessException.class, () -> employeeService.deleteEmployee(id));
+        doThrow(EmptyResultDataAccessException.class).when(employeeRepository).deleteById(1);
+        assertThrows(EmptyResultDataAccessException.class, () -> employeeService.deleteEmployee(1));
     }
 }
