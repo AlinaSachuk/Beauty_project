df3734ae5644d0f50569029ef1915babdb304ffe
AlinaSachuk <alinko-91@yandex.ru>
2023-05-07 13:19:34 +0300

    Tests: get, getAll, delete for Customer created.

--- src/test/java/com/beauty_project/service/CustomerServiceImplTest.java
+++ src/test/java/com/beauty_project/service/CustomerServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.beauty_project.service;
 
+import com.beauty_project.Utils;
 import com.beauty_project.domain.Customer;
 import com.beauty_project.repository.CustomerRepository;
 import com.beauty_project.service.impl.CustomerServiceImpl;
@@ -11,12 +12,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.webjars.NotFoundException;
 
+import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class CustomerServiceImplTest {
@@ -24,27 +25,38 @@ public class CustomerServiceImplTest {
     private CustomerRepository customerRepository;
     @InjectMocks
     private CustomerServiceImpl customerService;
-    private int id;
-    private Customer customer;
-    private List<Customer> allCustomers;
 
     @Test
     public void testGetByIdWithNotFoundException() {
-        doThrow(NotFoundException.class).when(customerRepository).findById(id);
-        assertThrows(NotFoundException.class, () -> customerService.getCustomerById(id));
+        doThrow(NotFoundException.class).when(customerRepository).findById(1);
+        assertThrows(NotFoundException.class, () -> customerService.getCustomerById(1));
     }
 
     @Test
     public void testGetAllCustomers() {
-        when(customerRepository.findAll()).thenReturn(allCustomers);
-        List<Customer> returnedCustomers = customerService.getAllCustomers();
-        verify(customerRepository).findAll();
-        assertEquals(allCustomers, returnedCustomers);
+        Customer customer1 = Utils.createCustomer(1, "Bob", Date.valueOf("2000-09-18"), "80449871254"
+                , "bob@gmail.com", "bob1809", "NULL", "bob1809");
+        Customer customer2 = Utils.createCustomer(2, "Bob", Date.valueOf("2000-09-18"), "80449871254"
+                , "bob@gmail.com", "bob1809", "NULL", "bob1809");
+        List<Customer> returnedCustomers = new ArrayList<>();
+        returnedCustomers.add(customer1);
+        returnedCustomers.add(customer2);
+        when(customerRepository.findAll()).thenReturn(returnedCustomers);
+        List<Customer> actual = customerRepository.findAll();
+        verify(customerRepository, times(1)).findAll();
+        assertNotNull(actual);
+        assertEquals(2, actual.size());
+    }
+
+    @Test
+    public void testDeleteCustomerById() {
+        customerService.deleteCustomer(1);
+        verify(customerRepository, times(1)).deleteById(1);
     }
 
     @Test
     public void testDeleteCustomerByIdWithEmptyResultDataAccessException() {
-        doThrow(EmptyResultDataAccessException.class).when(customerRepository).deleteById(id);
-        assertThrows(EmptyResultDataAccessException.class, () -> customerService.deleteCustomer(id));
+        doThrow(EmptyResultDataAccessException.class).when(customerRepository).deleteById(1);
+        assertThrows(EmptyResultDataAccessException.class, () -> customerService.deleteCustomer(1));
     }
 }
