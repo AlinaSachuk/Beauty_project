1d7f7fb3a21e95862d32c71b8e78b6c02749c048
AlinaSachuk <alinko-91@yandex.ru>
2023-05-07 13:53:18 +0300

    Tests: get, getAll, delete for Visit created.

--- src/test/java/com/beauty_project/service/VisitServiceImplTest.java
+++ src/test/java/com/beauty_project/service/VisitServiceImplTest.java
@@ -1,6 +1,6 @@
 package com.beauty_project.service;
 
-import com.beauty_project.domain.Customer;
+import com.beauty_project.Utils;
 import com.beauty_project.domain.Visit;
 import com.beauty_project.repository.CustomerRepository;
 import com.beauty_project.repository.VisitRepository;
@@ -13,6 +13,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.webjars.NotFoundException;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -20,6 +21,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -31,26 +33,27 @@ public class VisitServiceImplTest {
     private CustomerRepository customerRepository;
     @InjectMocks
     private VisitServiceImpl visitService;
-    private int id;
-    private Visit visit;
-    private Customer customer;
-    private ArrayList<Visit> allVisits;
 
     @Test
     public void testGetByIdWithNotFoundException() {
-        doThrow(NotFoundException.class).when(visitRepository).findById(id);
-        assertThrows(NotFoundException.class, () -> visitService.getVisitById(id));
+        doThrow(NotFoundException.class).when(visitRepository).findById(1);
+        assertThrows(NotFoundException.class, () -> visitService.getVisitById(1));
     }
 
     @Test
     public void testGetAllVisits() {
+        Visit visit1 = Utils.createVisit(1, Date.valueOf("2023-05-07"), 100.0, 1);
+        Visit visit2 = Utils.createVisit(2, Date.valueOf("2023-05-07"), 100.0, 1);
+        List<Visit> allVisits = new ArrayList<>();
+        allVisits.add(visit1);
+        allVisits.add(visit2);
         when(visitRepository.findAll()).thenReturn(allVisits);
-        List<Visit> returnedVisits = visitService.getAllVisits();
-        verify(visitRepository).findAll();
-        assertEquals(allVisits, returnedVisits);
+        List<Visit> actual = visitRepository.findAll();
+        verify(visitRepository, times(1)).findAll();
+        assertEquals(2, actual.size());
     }
 
-    @Test
+    /*@Test
     public void testGetAllVisitsForSingleCustomer(){
         when(customerRepository.findById(id)).thenReturn(Optional.of(customer));
         //Customer returnedCustomer =
@@ -58,11 +61,17 @@ public class VisitServiceImplTest {
         List<Visit> returnedVisits = visitService.getAllVisitsForSingleCustomer(customer.getId());
         verify(visitRepository).findAll();
         assertEquals(allVisits, returnedVisits);
+    }*/
+
+    @Test
+    public void testDeleteVisitById() {
+        visitService.deleteVisit(1);
+        verify(visitRepository, times(1)).deleteById(1);
     }
 
     @Test
     public void testDeleteVisitByIdWithEmptyResultDataAccessException() {
-        doThrow(EmptyResultDataAccessException.class).when(visitRepository).deleteById(id);
-        assertThrows(EmptyResultDataAccessException.class, () -> visitService.deleteVisit(id));
+        doThrow(EmptyResultDataAccessException.class).when(visitRepository).deleteById(1);
+        assertThrows(EmptyResultDataAccessException.class, () -> visitService.deleteVisit(1));
     }
 }
