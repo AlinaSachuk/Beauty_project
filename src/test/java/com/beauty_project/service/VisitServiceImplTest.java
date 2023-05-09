507eb36a60246db3cd1db519c3628b9efcad6f89
AlinaSachuk <alinko-91@yandex.ru>
2023-05-09 19:14:24 +0300

    Tests for getById, create, update methods done.

--- src/test/java/com/beauty_project/service/VisitServiceImplTest.java
+++ src/test/java/com/beauty_project/service/VisitServiceImplTest.java
@@ -2,7 +2,10 @@ package com.beauty_project.service;
 
 import com.beauty_project.Utils;
 import com.beauty_project.domain.Visit;
+import com.beauty_project.domain.request.VisitRequestDto;
+import com.beauty_project.domain.response.VisitResponseDto;
 import com.beauty_project.repository.CustomerRepository;
+import com.beauty_project.repository.StatusRepository;
 import com.beauty_project.repository.VisitRepository;
 import com.beauty_project.service.impl.VisitServiceImpl;
 import org.junit.jupiter.api.Test;
@@ -18,8 +21,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -31,6 +36,8 @@ public class VisitServiceImplTest {
     private VisitRepository visitRepository;
     @Mock
     private CustomerRepository customerRepository;
+    @Mock
+    StatusRepository statusRepository;
     @InjectMocks
     private VisitServiceImpl visitService;
 
@@ -38,6 +45,15 @@ public class VisitServiceImplTest {
     public void testGetByIdWithNotFoundException() {
         doThrow(NotFoundException.class).when(visitRepository).findById(1);
         assertThrows(NotFoundException.class, () -> visitService.getVisitById(1));
+    }
+
+    @Test
+    public void testGetById() {
+        var visit = Utils.createVisit(1, Date.valueOf("2023-05-07"), 100.0, 1);
+        when(visitRepository.findById(1)).thenReturn(Optional.of(visit));
+        VisitResponseDto actual = visitService.getVisitById(1);
+        assertNotNull(actual);
+        assertEquals(new VisitResponseDto(1, Date.valueOf("2023-05-07"), 100.0, 1), actual);
     }
 
     @Test
@@ -53,15 +69,33 @@ public class VisitServiceImplTest {
         assertEquals(2, actual.size());
     }
 
-    /*@Test
-    public void testGetAllVisitsForSingleCustomer(){
-        when(customerRepository.findById(id)).thenReturn(Optional.of(customer));
-        //Customer returnedCustomer =
-        when(visitRepository.findVisitsByCustomerId(id)).thenReturn(allVisits);
-        List<Visit> returnedVisits = visitService.getAllVisitsForSingleCustomer(customer.getId());
-        verify(visitRepository).findAll();
-        assertEquals(allVisits, returnedVisits);
-    }*/
+    @Test
+    public void testSave() {
+        var visit = Utils.createVisit(1, Date.valueOf("2023-05-07"), 100.0, 1);
+        var customer = Utils.createCustomer(1, "Bob", Date.valueOf("2000-05-09")
+                , "80291235689", "bob2000@gmail.com", "bob2000"
+                , "SILVER", "bob2000");
+        var status = Utils.createStatus("SILVER", 5, 1);
+        when(customerRepository.findById(1)).thenReturn(Optional.of(customer));
+        when(statusRepository.findPercentByStatus("SILVER")).thenReturn(5);
+        when(visitRepository.save(any())).thenReturn(visit);
+        VisitResponseDto actual = visitService.createVisit(new VisitRequestDto(1, Date.valueOf("2023-05-07")
+                , 100.0, 1), 1);
+        assertNotNull(actual);
+        assertEquals(new VisitResponseDto(1, Date.valueOf("2023-05-07"), 100.0, 1), actual);
+    }
+
+    @Test
+    public void testUpdate() {
+        var savedVisit = Utils.createVisit(1, Date.valueOf("2023-05-07"), 100.0, 1);
+        var updatedVisit = Utils.createVisit(1, Date.valueOf("2023-05-09"), 110.0, 1);
+        when(visitRepository.findById(1)).thenReturn(Optional.of(savedVisit));
+        when(visitRepository.save(any())).thenReturn(updatedVisit);
+        VisitResponseDto actual = visitService.updateVisit(new VisitRequestDto(1, Date.valueOf("2023-05-07")
+                , 100.0, 1));
+        assertNotNull(actual);
+        assertEquals(new VisitResponseDto(1, Date.valueOf("2023-05-09"), 110.0, 1), actual);
+    }
 
     @Test
     public void testDeleteVisitById() {
