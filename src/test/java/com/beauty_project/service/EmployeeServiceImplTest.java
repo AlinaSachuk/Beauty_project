af3a0d0fb11a75a4b47ebd6c73bcd0d7ca672a0a
AlinaSachuk <alinko-91@yandex.ru>
2023-05-09 18:04:33 +0300

    Tests for getById, create, update methods done.

--- src/test/java/com/beauty_project/service/EmployeeServiceImplTest.java
+++ src/test/java/com/beauty_project/service/EmployeeServiceImplTest.java
@@ -2,6 +2,8 @@ package com.beauty_project.service;
 
 import com.beauty_project.Utils;
 import com.beauty_project.domain.Employee;
+import com.beauty_project.domain.request.EmployeeRequestDto;
+import com.beauty_project.domain.response.EmployeeResponseDto;
 import com.beauty_project.repository.EmployeeRepository;
 import com.beauty_project.service.impl.EmployeeServiceImpl;
 import org.junit.jupiter.api.Test;
@@ -14,8 +16,12 @@ import org.webjars.NotFoundException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -35,9 +41,22 @@ public class EmployeeServiceImplTest {
     }
 
     @Test
+    public void testGetById() {
+        var employee = Utils.createEmployee(1, "Anna", "doctor"
+                , "BSMU", "since 2015");
+        when(employeeRepository.findById(1)).thenReturn(Optional.of(employee));
+        EmployeeResponseDto actual = employeeService.getEmployeeById(1);
+        assertNotNull(actual);
+        assertEquals(new EmployeeResponseDto(1, "Anna", "doctor"
+                , "BSMU", "since 2015"), actual);
+    }
+
+    @Test
     public void testGetAllEmployees() {
-        Employee employee1 = Utils.createEmployee(1, "Anna", "doctor", "BSMU", "since 2015");
-        Employee employee2 = Utils.createEmployee(2, "Anna", "doctor", "BSMU", "since 2015");
+        Employee employee1 = Utils.createEmployee(1, "Anna", "doctor"
+                , "BSMU", "since 2015");
+        Employee employee2 = Utils.createEmployee(2, "Anna", "doctor"
+                , "BSMU", "since 2015");
         List<Employee> returnedEmployees = new ArrayList<>();
         returnedEmployees.add(employee1);
         returnedEmployees.add(employee2);
@@ -45,6 +64,33 @@ public class EmployeeServiceImplTest {
         List<Employee> actual = employeeRepository.findAll();
         verify(employeeRepository, times(1)).findAll();
         assertEquals(2, actual.size());
+    }
+
+    @Test
+    public void testSave() {
+        var employee = Utils.createEmployee(1, "Anna", "doctor"
+                , "BSMU", "since 2015");
+        when(employeeRepository.save(any())).thenReturn(employee);
+        EmployeeResponseDto actual = employeeService.createEmployee(new EmployeeRequestDto(
+                1, "Anna", "doctor", "BSMU", "since 2015"));
+        assertNotNull(actual);
+        assertEquals(new EmployeeResponseDto(1, "Anna", "doctor"
+                , "BSMU", "since 2015"), actual);
+    }
+
+    @Test
+    public void testUpdate() {
+        var savedEmployee = Utils.createEmployee(1, "Anna", "doctor"
+                , "BSMU", "since 2015");
+        var updatedEmployee = Utils.createEmployee(1, "Hanna", "doctor"
+                , "BSMU", "since 2015");
+        when(employeeRepository.findById(1)).thenReturn(Optional.of(savedEmployee));
+        when(employeeRepository.save(any())).thenReturn(updatedEmployee);
+        EmployeeResponseDto actual = employeeService.updateEmployee(new EmployeeRequestDto(
+                1, "Anna", "doctor", "BSMU", "since 2015"));
+        assertNotNull(actual);
+        assertEquals(new EmployeeResponseDto(1, "Hanna", "doctor"
+                , "BSMU", "since 2015"), actual);
     }
 
     @Test
