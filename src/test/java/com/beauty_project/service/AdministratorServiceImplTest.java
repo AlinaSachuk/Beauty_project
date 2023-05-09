3d548ade9877ddea3829badeeee1c03d96b8dd8a
AlinaSachuk <alinko-91@yandex.ru>
2023-05-09 13:10:47 +0300

    Test for get by login method created

--- src/test/java/com/beauty_project/service/AdministratorServiceImplTest.java
+++ src/test/java/com/beauty_project/service/AdministratorServiceImplTest.java
@@ -1,5 +1,7 @@
 package com.beauty_project.service;
 
+import com.beauty_project.Utils;
+import com.beauty_project.domain.response.AdministratorResponseDto;
 import com.beauty_project.repository.AdministratorRepository;
 import com.beauty_project.service.impl.AdministratorServiceImpl;
 import org.junit.jupiter.api.Test;
@@ -10,10 +12,15 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.webjars.NotFoundException;
 
+import java.util.Optional;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class AdministratorServiceImplTest {
@@ -27,6 +34,15 @@ public class AdministratorServiceImplTest {
     public void testGetByLoginWithNotFoundException() {
         doThrow(NotFoundException.class).when(administratorRepository).findAdministratorByLogin("Admin");
         assertThrows(NotFoundException.class, () -> administratorService.getAdminByLogin("Admin"));
+    }
+
+    @Test
+    public void testGetByLogin() {
+        var admin = Utils.createAdministrator(1, "Admin", "admin", "ADMIN");
+        when(administratorRepository.findAdministratorByLogin("Admin")).thenReturn(Optional.of(admin));
+        AdministratorResponseDto actual = administratorService.getAdminByLogin("Admin");
+        assertNotNull(actual);
+        assertEquals(new AdministratorResponseDto(1, "Admin"), actual);
     }
 
     @Test
