8892ca84a547e143f045d39e362add37c056dff8
AlinaSachuk <alinko-91@yandex.ru>
2023-05-14 22:34:18 +0300

    Swagger: added to Visit Controller

--- src/main/java/com/beauty_project/controller/VisitController.java
+++ src/main/java/com/beauty_project/controller/VisitController.java
@@ -3,6 +3,10 @@ package com.beauty_project.controller;
 import com.beauty_project.domain.request.VisitRequestDto;
 import com.beauty_project.domain.response.VisitResponseDto;
 import com.beauty_project.service.VisitService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,6 +22,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "401", description = "Don`t have authorization"),
+        @ApiResponse(responseCode = "403", description = "Don`t have authority"),
+        @ApiResponse(responseCode = "404", description = "Resource not found")
+})
 @RequestMapping("/visit")
 public class VisitController {
     private final VisitService visitService;
@@ -28,30 +37,49 @@ public class VisitController {
     }
 
     @GetMapping("/{id}")
+    @Operation(description = "Finding visit by id.")
+    @ApiResponse(responseCode = "200", description = "Visit found by id successfully.")
     @ResponseStatus(HttpStatus.OK)
     public VisitResponseDto getVisitById(@PathVariable int id) {
         return visitService.getVisitById(id);
     }
 
     @GetMapping("/getAll")
+    @Operation(description = "Finding all visits.")
+    @ApiResponse(responseCode = "200", description = "All visits found successfully.")
     @ResponseStatus(HttpStatus.OK)
     public List<VisitResponseDto> getAllVisits() {
         return visitService.getAllVisits();
     }
 
     @PostMapping("/{id}")
+    @Operation(description = "Creating visit by customer`s id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Visit created successfully."),
+            @ApiResponse(responseCode = "400", description = "Incorrect price entered."),
+            @ApiResponse(responseCode = "404", description = "Customer not found.")
+    })
     @ResponseStatus(HttpStatus.CREATED)
-    public VisitResponseDto createVisit(@RequestBody VisitRequestDto visitDto, @PathVariable int id) {
+    public VisitResponseDto createVisit(@RequestBody VisitRequestDto visitDto
+            , @Parameter(description = "This is customer`s id you need to enter to create visit")
+                                            @PathVariable int id) {
         return visitService.createVisit(visitDto, id);
     }
 
     @PutMapping
+    @Operation(description = "Updating information about visit.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Visit`s info updated successfully."),
+            @ApiResponse(responseCode = "400", description = "Incorrect price entered.")
+    })
     @ResponseStatus(HttpStatus.OK)
     public VisitResponseDto updateVisit(@RequestBody VisitRequestDto visitDto) {
         return visitService.updateVisit(visitDto);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(description = "Deleting visit by id.")
+    @ApiResponse(responseCode = "204", description = "Visit deleted by id successfully.")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteVisit(@PathVariable int id) {
         visitService.deleteVisit(id);
