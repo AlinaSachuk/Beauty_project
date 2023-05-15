0a8c15597bc51443e32b2bda7980153629540efc
AlinaSachuk <alinko-91@yandex.ru>
2023-05-15 09:34:49 +0300

    Swagger: added to Status Controller

--- src/main/java/com/beauty_project/controller/StatusController.java
+++ src/main/java/com/beauty_project/controller/StatusController.java
@@ -3,6 +3,9 @@ package com.beauty_project.controller;
 import com.beauty_project.domain.request.StatusRequestDto;
 import com.beauty_project.domain.response.StatusResponseDto;
 import com.beauty_project.service.StatusService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,6 +21,10 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "401", description = "Don`t have authorization"),
+        @ApiResponse(responseCode = "403", description = "Don`t have authority")
+})
 @RequestMapping("/status")
 public class StatusController {
     private final StatusService statusService;
@@ -28,30 +35,52 @@ public class StatusController {
     }
 
     @GetMapping("/{id}")
+    @Operation(description = "Finding status by id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Status found by id successfully."),
+            @ApiResponse(responseCode = "404", description = "Status by entered id not found.")
+    })
     @ResponseStatus(HttpStatus.OK)
     public StatusResponseDto getStatusById(@PathVariable int id) {
         return statusService.getStatusById(id);
     }
 
     @GetMapping
+    @Operation(description = "Finding all statuses.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "All statuses found successfully"),
+            @ApiResponse(responseCode = "404", description = "Status not found.")
+    })
     @ResponseStatus(HttpStatus.OK)
     public List<StatusResponseDto> getAllStatus() {
         return statusService.getAllStatus();
     }
 
     @PostMapping
+    @Operation(description = "Creating status")
+    @ApiResponse(responseCode = "201", description = "Status created successfully.")
     @ResponseStatus(HttpStatus.CREATED)
     public StatusResponseDto createStatus(@RequestBody StatusRequestDto statusDto) {
         return statusService.createStatus(statusDto);
     }
 
     @PutMapping
+    @Operation(description = "Updating status.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Status updated successfully."),
+            @ApiResponse(responseCode = "404", description = "Requested status not found.")
+    })
     @ResponseStatus(HttpStatus.OK)
     public StatusResponseDto updateStatus(@RequestBody StatusRequestDto statusDto) {
         return statusService.updateStatus(statusDto);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(description = "Deleting status by id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Visit deleted by id successfully."),
+            @ApiResponse(responseCode = "404", description = "Requested status not found.")
+    })
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteStatus(@PathVariable int id) {
         statusService.deleteStatus(id);
