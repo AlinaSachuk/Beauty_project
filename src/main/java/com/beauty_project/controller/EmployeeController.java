6d720d32895b2eaec7f460641c38ccdfaf8d8a15
AlinaSachuk <alinko-91@yandex.ru>
2023-05-15 15:18:12 +0300

    Swagger: added to Employee Controller

--- src/main/java/com/beauty_project/controller/EmployeeController.java
+++ src/main/java/com/beauty_project/controller/EmployeeController.java
@@ -3,6 +3,9 @@ package com.beauty_project.controller;
 import com.beauty_project.domain.request.EmployeeRequestDto;
 import com.beauty_project.domain.response.EmployeeResponseDto;
 import com.beauty_project.service.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,6 +21,10 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "401", description = "Don`t have authorization"),
+        @ApiResponse(responseCode = "403", description = "Don`t have authority")
+})
 @RequestMapping("/employee")
 public class EmployeeController {
 
@@ -29,30 +36,52 @@ public class EmployeeController {
     }
 
     @GetMapping("/{id}")
+    @Operation(description = "Finding employee by id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Employee found by id successfully."),
+            @ApiResponse(responseCode = "404", description = "Employee by entered id not found.")
+    })
     @ResponseStatus(HttpStatus.OK)
     public EmployeeResponseDto getEmployeeById(@PathVariable int id) {
         return employeeService.getEmployeeById(id);
     }
 
     @GetMapping
+    @Operation(description = "Finding all employees.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "All employees found successfully"),
+            @ApiResponse(responseCode = "404", description = "Employees not found.")
+    })
     @ResponseStatus(HttpStatus.OK)
     public List<EmployeeResponseDto> getAllEmployees() {
         return employeeService.getAllEmployees();
     }
 
     @PostMapping
+    @Operation(description = "Creating employee")
+    @ApiResponse(responseCode = "201", description = "Employee created successfully.")
     @ResponseStatus(HttpStatus.CREATED)
     public EmployeeResponseDto createEmployee(@RequestBody EmployeeRequestDto employeeDto) {
         return employeeService.createEmployee(employeeDto);
     }
 
     @PutMapping
+    @Operation(description = "Updating information about employee.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Employee`s information updated successfully."),
+            @ApiResponse(responseCode = "404", description = "Requested employee not found.")
+    })
     @ResponseStatus(HttpStatus.OK)
     public EmployeeResponseDto updateEmployee(@RequestBody EmployeeRequestDto employeeDto) {
         return employeeService.updateEmployee(employeeDto);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(description = "Deleting employee by id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Employee deleted by id successfully."),
+            @ApiResponse(responseCode = "404", description = "Requested employee not found.")
+    })
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteEmployee(@PathVariable int id) {
         employeeService.deleteEmployee(id);
