8378ac0cdedbd5e5ecd9214aa4983d2e4694a852
AlinaSachuk <alinko-91@yandex.ru>
2023-05-15 15:19:17 +0300

    Swagger: added to Administrator Controller

--- src/main/java/com/beauty_project/controller/AdministratorController.java
+++ src/main/java/com/beauty_project/controller/AdministratorController.java
@@ -3,6 +3,9 @@ package com.beauty_project.controller;
 import com.beauty_project.domain.request.RegistrationAdminDto;
 import com.beauty_project.domain.response.AdministratorResponseDto;
 import com.beauty_project.service.AdministratorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +15,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "401", description = "Don`t have authorization"),
+        @ApiResponse(responseCode = "403", description = "Don`t have authority")
+})
 @RequestMapping("/admin")
 public class AdministratorController {
     private final AdministratorService administratorService;
@@ -22,6 +29,8 @@ public class AdministratorController {
     }
 
     @PostMapping("/registration")
+    @Operation(description = "Creating administrator")
+    @ApiResponse(responseCode = "201", description = "Administrator created successfully.")
     @ResponseStatus(HttpStatus.CREATED)
     public AdministratorResponseDto createAdmin(@RequestBody RegistrationAdminDto adminDto) {
         return administratorService.createAdmin(adminDto);
